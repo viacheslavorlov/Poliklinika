c1bd0f91eb8aff996c3004392e9d4728344ebd12
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-02-23 21:09:07 +0300

    no info for patient

---
+++ script.js
@@ -0,0 +1,88 @@
+'use strict';
+//переменные
+const patientName = document.getElementById('patientName');
+const otherComplains = document.querySelector('#other');
+const anamnez = document.querySelector('#hystory');
+const perObl = document.querySelector('#per-obl');
+const perOblAdd = document.querySelector('#per-obl-add');
+const opisanieUzlov = document.querySelector('#opisanie-uzlov');
+const obrazovania = document.querySelector('#obrazovania');
+const anoskopia = document.querySelector('#anoskopia');
+const diagnoz = document.querySelector('#diagnoz');
+const diagnoz2 = document.querySelector('#diagnoz2');
+const ln = document.querySelector('#trud');
+const lnNachalo = document.querySelector('#ln-nachalo');
+const lnKonec = document.querySelector('#ln-konec');
+const datLn = document.querySelector('#dat-ln');
+const otherObsledovania = document.querySelector('#other-obsledovania');
+const otherLechenie = document.querySelector('#other-lechenie');
+const date = document.querySelector('#date');
+const obrazovaniaConteiner = document.querySelector('#obrazovania-conteiner');
+const prepareForPrintButton = document.querySelector('#before-print');
+
+//установить дату приёма на сегодняшний день
+date.valueAsDate = new Date();
+
+// ! Блок для пациента.
+
+
+
+function addCssForPrint() {
+    if (ln.value === 'na-rabotu') {
+        datLn.parentElement.style.display = 'none';
+        ln.parentElement.style.display = 'none';
+    }
+    if (otherLechenie.value === '') {
+        otherLechenie.parentElement.style.display = 'none';
+    }
+    if (otherObsledovania.value === '') {
+        otherObsledovania.parentElement.style.display = 'none';
+    }
+    if (diagnoz.value !== '') {
+        diagnoz2.parentElement.style.display = 'none';
+    } else {
+        diagnoz.parentElement.style.display = 'none';
+    }
+    if (otherObsledovania.value === '') {
+        otherObsledovania.parentElement.style.display = 'none';
+    }
+    if (anoskopia.value === '') {
+        anoskopia.parentElement.style.display = 'none';
+    }
+    if (obrazovania.value === '') {
+        obrazovania.value = 'нет';
+    }
+    if (opisanieUzlov.value === '') {
+        opisanieUzlov.innerHTML = 'Геморроидальные узлы на 3, 7, 11 часах: не выраженные геморроидальные узлы.';
+    } else {
+        opisanieUzlov.value = `Геморроидальные узлы на 3, 7, 11 часах:  ${opisanieUzlov.value}`;
+    }
+    if (perObl.value === 'norm') {
+        perOblAdd.style.display = 'none';
+    }
+    if (obrazovania.value === '') {
+        obrazovaniaConteiner.style.flexDirection = 'row';
+    }
+    if (otherComplains.value === '') {
+        otherComplains.parentElement.style.display = 'none';
+    }
+    /*
+    * функция для рпазворота даты ЛН в формат дд.мм.гггг.
+    */
+    
+    lnNachalo.value = lnNachalo.value.split('-').reverse().join('.');
+    lnKonec.value = lnKonec.value.split('-').reverse().join('.');
+
+}
+
+
+prepareForPrintButton.addEventListener('click', function (e) {
+    addCssForPrint();
+    window.print();
+});
+
+document.addEventListener('keydown', function (e) {
+    if (e.code === 'KeyP' && e.ctrlKey) {
+        addCssForPrint();
+    }
+});
