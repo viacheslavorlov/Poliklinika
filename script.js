93a638d552760c871f20d880c5e96561cdfc4bb6
viacheslavorlov <viacheslavorlov@gmail.com>
2022-03-27 14:17:15 +0300

    text result done

--- script.js
+++ script.js
@@ -21,6 +21,12 @@ const date = document.querySelector('#date');
 const obrazovaniaConteiner = document.querySelector('#obrazovania-conteiner');
 const prepareForPrintButton = document.querySelector('#before-print');
 const forPatient = document.querySelector('.for-patient');
+const forCart = document.querySelector('.for-cart');
+const tonus = document.querySelector('#tonus');
+const condition = document.querySelector('#condition');
+const stul = document.querySelector('#stul');
+
+
 
 //установить дату приёма на сегодняшний день
 date.valueAsDate = new Date();
@@ -51,6 +57,41 @@ function recomendationList() {
 }
 
 
+function textForCart() {
+    const complains = document.querySelectorAll('.complains'),
+          uzly = document.querySelector('#uzly');
+        let listOfComplains = [];
+        complains.forEach(function(item) {
+            if (item.checked) {
+                listOfComplains.push(item.value);
+            }
+        });
+        listOfComplains.push(otherComplains.value);
+        
+    
+    forCart.innerHTML = `
+        <h2>Центр Репродуктивного Здоровья</h2>
+        <p><b>Прием колопроктолога.</b></p>
+        <p><i>ул.Рахманинова д. 10. тел. 78-78-20, 78-20-20.</i></p>
+        <p> Дата: ${date.value.split('-').reverse().join('.')}</p>
+        <p>Пациент: ${patientName.value} Дата рождения: ${birthday.value.split('-').reverse().join('.')}</p>
+        <div>Жалобы: ${listOfComplains. join(', ')}</div>
+        <div>${anamnez.value}</div>
+        <div>Состояние: ${condition.value}</div>
+        <div>Стул: ${stul.value}</div>
+        <div>Перианальная область: ${perObl.value + ' ' + ((perOblAdd.value == '') ? '' : perOblAdd.value)}</div>
+        <div>Тонус сфинктера: ${tonus.value}</div>       
+        <div>${opisanieUzlov.value}</div>
+        <div>Выпадение геморроидальных узлов: ${uzly.value}</div>
+        <div>Дополнительные образования: ${obrazovania.value}</label>
+        <div>Аноскопия: ${anoskopia.value}</div>
+        <p><bolb>Диагноз: ${(diagnoz.value == '') ? diagnoz2.value : diagnoz.value + ' ' + diagnoz2.value}</bolb></p>
+        <p>Рекомендации:</p>
+        <div>${recomendationList()}</div>
+        <p><i> врач: Орлов В.И. </i></p >
+    `;
+}
+
 function addInfoForPatient() {
 
     forPatient.innerHTML =
@@ -59,7 +100,7 @@ function addInfoForPatient() {
         <p><i>ул.Рахманинова д. 10. тел. 78-78-20, 78-20-20.</i></p>
         <p> Дата: ${date.value.split('-').reverse().join('.')}</p>
         <p>Пациент: ${patientName.value} Дата рождения: ${birthday.value.split('-').reverse().join('.')}</p>
-        <p>Диагноз: ${(diagnoz.value == '') ? diagnoz2.value : diagnoz.value + ' ' + diagnoz2.value}</p>
+        <p><bold>Диагноз: ${(diagnoz.value == '') ? diagnoz2.value : diagnoz.value + ' ' + diagnoz2.value}</bold></p>
         <p>Рекомендации:</p>
         <div><b>${recomendationList()}</b></div>
         <p><i> врач: Орлов В.И. </i></p >
@@ -72,31 +113,43 @@ function addCssForPrint() {
         datLn.parentElement.style.display = 'none';
         ln.parentElement.style.display = 'none';
     }
+    
     if (otherLechenie.value === '') {
         otherLechenie.parentElement.style.display = 'none';
     }
+    
     if (otherObsledovania.value === '') {
         otherObsledovania.parentElement.style.display = 'none';
     }
+    
     if (diagnoz2.value === '') {
         diagnoz2.parentElement.style.display = 'none';
     }
+    
     if (otherObsledovania.value === '') {
         otherObsledovania.parentElement.style.display = 'none';
     }
+    
     if (anoskopia.value === '') {
         anoskopia.parentElement.style.display = 'none';
     }
+    
     if (obrazovania.value === '') {
         obrazovania.value = 'нет';
     }
+
     if (opisanieUzlov.value === '') {
         opisanieUzlov.innerHTML = 'Геморроидальные узлы на 3, 7, 11 часах: не выраженные геморроидальные узлы.';
     } else {
-        opisanieUzlov.value = `Геморроидальные узлы на 3, 7, 11 часах:  ${opisanieUzlov.value}`;
+        opisanieUzlov.value = `Геморроидальные узлы на 3, 7, 11 часах: ${opisanieUzlov.value}`;
     }
-    if (perObl.value === 'norm') {
+
+    if (perObl.value === 'не изменена') {
         perOblAdd.style.display = 'none';
+    }
+
+    if (perOblAdd.value === '') {
+        perOblAdd.parentElement.style.display = 'none';
     }
     if (obrazovania.value === '') {
         obrazovaniaConteiner.style.flexDirection = 'row';
@@ -114,13 +167,17 @@ function addCssForPrint() {
 
 prepareForPrintButton.addEventListener('click', function (e) {
     addCssForPrint();
+    textForCart();
     addInfoForPatient();
+    document.querySelector('main').style.display = 'none';
     window.print();
 });
 
 document.addEventListener('keydown', function (e) {
     if (e.code === 'KeyP' && e.ctrlKey) {
         addCssForPrint();
+        textForCart();
+        document.querySelector('main').style.display = 'none';
         addInfoForPatient();
     }
 });
