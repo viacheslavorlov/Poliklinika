8d12a19df3f39c65af2a88db8f8a552c1bf6cec2
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-04-12 13:18:40 +0300

    for NOKB

--- scripts/priem.js
+++ scripts/priem.js
@@ -12,7 +12,6 @@ const obrazovania = document.querySelector('#obrazovania');
 const anoskopia = document.querySelector('#anoskopia');
 const diagnoz = document.querySelector('#diagnoz');
 const diagnoz2 = document.querySelector('#diagnoz2');
-const ln = document.querySelector('#trud');
 const lnNachalo = document.querySelector('#ln-nachalo');
 const lnKonec = document.querySelector('#ln-konec');
 const datLn = document.querySelector('#dat-ln');
@@ -26,8 +25,7 @@ const forCart = document.querySelector('.for-cart');
 const tonus = document.querySelector('#tonus');
 const condition = document.querySelector('#condition');
 const stul = document.querySelector('#stul');
-
-
+const naPerchatke = document.querySelector('#na-perchatke');
 
 //установить дату приёма на сегодняшний день
 date.valueAsDate = new Date();
@@ -48,15 +46,12 @@ function recomendationList() {
         }
     }
 
-    if (ln.value === 'Нетрудоспособен.') {
-        list.push([`${ln.value} ${datLn.value} больничный лист
-        c ${lnNachalo.value.split('-').reverse().join('.')} по ${lnKonec.value.split('-').reverse().join('')}.
-        Контрольная явка на ${lnKonec.value.split('-').reverse().join('')}`]);
-
-    }
     return list.join('<br>');
 }
 
+function addCssForPrint() {
+    document.querySelector('main').style.display = 'none';
+}
 
 function textForCart() {
     const complains = document.querySelectorAll('.complains'),
@@ -89,8 +84,8 @@ function textForCart() {
         </div>
         <div>Выпадение геморроидальных узлов: ${uzly.value}</div>
         <div>Дополнительные образования: ${obrazovania.value}</label>
-        <div>Аноскопия: ${anoskopia.value}</div>
-        <p><bolb>Диагноз: ${(diagnoz.value == '') ? diagnoz2.value : diagnoz.value + ' ' + diagnoz2.value}</bolb></p>
+        <div>На перчатке: ${naPerchatke.value}</div>
+        <p><b>Диагноз: ${(diagnoz.value == '') ? diagnoz2.value : diagnoz.value + ' ' + diagnoz2.value}</b></p>
         <p>Рекомендации:</p>
         <div>${recomendationList()}</div>
         <p><i> врач: Орлов В.И. </i></p >
@@ -98,7 +93,6 @@ function textForCart() {
 }
 
 function addInfoForPatient() {
-
     forPatient.innerHTML =
         `
         <h2><b>Прием колопроктолога.</b></h2>
@@ -112,68 +106,13 @@ function addInfoForPatient() {
         `;
 }
 
-function addCssForPrint() {
-    // if (ln.value === 'Трудоспособен. ЛН не нужен.') {
-    //     datLn.parentElement.style.display = 'none';
-    //     ln.parentElement.style.display = 'none';
-    // }
-    
-    // if (otherLechenie.value === '') {
-    //     otherLechenie.parentElement.style.display = 'none';
-    // }
-    
-    // if (otherObsledovania.value === '') {
-    //     otherObsledovania.parentElement.style.display = 'none';
-    // }
-    
-    // if (diagnoz2.value === '') {
-    //     diagnoz2.parentElement.style.display = 'none';
-    // }
-    
-    // if (otherObsledovania.value === '') {
-    //     otherObsledovania.parentElement.style.display = 'none';
-    // }
-    
-    // if (anoskopia.value === '') {
-    //     anoskopia.parentElement.style.display = 'none';
-    // }
-    
-    if (obrazovania.value === '') {
-        obrazovania.value = 'нет';
-    }
 
-    // if (opisanieUzlov.value === '') {
-    //     opisanieUzlov.innerHTML = 'Геморроидальные узлы на 3, 7, 11 часах: не выраженные геморроидальные узлы.';
-    // } else {
-    //     opisanieUzlov.value = `Геморроидальные узлы на 3, 7, 11 часах: ${opisanieUzlov.value}`;
-    // }
-
-    // if (perObl.value === 'не изменена') {
-    //     perOblAdd.style.display = 'none';
-    // }
-
-    // if (perOblAdd.value === '') {
-    //     perOblAdd.parentElement.style.display = 'none';
-    // }
-    // if (obrazovania.value === '') {
-    //     obrazovaniaConteiner.style.flexDirection = 'row';
-    // }
-    // if (otherComplains.value === '') {
-    //     otherComplains.style.display = 'none';
-    // }
-
-    // * функция для рпазворота даты ЛН в формат дд.мм.гггг.
-
-    lnNachalo.value = lnNachalo.value.split('-').reverse().join('.');
-    lnKonec.value = lnKonec.value.split('-').reverse().join('.');
-}
 
 
 prepareForPrintButton.addEventListener('click', function (e) {
     addCssForPrint();
     textForCart();
     addInfoForPatient();
-    document.querySelector('main').style.display = 'none';
     window.print();
 });
 
@@ -181,7 +120,6 @@ document.addEventListener('keydown', function (e) {
     if (e.code === 'KeyP' && e.ctrlKey) {
         addCssForPrint();
         textForCart();
-        document.querySelector('main').style.display = 'none';
         addInfoForPatient();
     }
 });
